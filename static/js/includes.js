5fa04a2b14b1bc0f0ce0396902c1343cea96d267
Bryce Knight <bkni0201@uni.sydney.edu.au>
2024-05-19 21:59:07 +1000

    Added simple dark mode functionality, slightly extended report accessibility section

--- static/js/includes.js
+++ static/js/includes.js
@@ -17,3 +17,19 @@ if (username) {
     document.querySelector("#nav-logged-in").style.display = "none";
     document.querySelector("#nav-logged-out").style.display = "";
 }
+
+function lightMode() {
+    document.documentElement.style.setProperty("--color-bg-0", "#fdfaf8");
+    document.documentElement.style.setProperty("--color-prm-0", "#ce9a7c");
+    document.documentElement.style.setProperty("--color-sec-0", "#addca8");
+    document.documentElement.style.setProperty("--color-acc-0", "#57c985");
+    document.documentElement.style.setProperty("--color-txt-0", "#140d07");
+}
+
+function darkMode() {
+    document.documentElement.style.setProperty("--color-bg-0", "#080502");
+    document.documentElement.style.setProperty("--color-prm-0", "#824e30");
+    document.documentElement.style.setProperty("--color-sec-0", "#285823");
+    document.documentElement.style.setProperty("--color-acc-0", "#37a965");
+    document.documentElement.style.setProperty("--color-txt-0", "#f8f2ec");
+}
